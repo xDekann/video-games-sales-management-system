54811b050322958e9fc8d34c0345b417f7de7f69
xDekann <oozog255@gmail.com>
2023-08-05 13:41:53 +0200

    Spring initialzr init

---
+++ vgsms/src/main/java/com/pk/vgsms/VgsmsApplication.java
@@ -0,0 +1,13 @@
+package com.pk.vgsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VgsmsApplication.class, args);
+	}
+
+}
---
+++ vgsms/src/test/java/com/pk/vgsms/VgsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.vgsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VgsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
